aa1ded86378093a289dc9db59bfa5eede9d5e5f7
XxWhitexX <xxwhitexx.acc@gmail.com>
2024-12-10 23:02:36 -0300

    Fixed navbar

--- src/components/global/navbar/nav-bar.tsx
+++ src/components/global/navbar/nav-bar.tsx
@@ -2,50 +2,66 @@
 
 import Link from 'next/link';
 
-import { Container } from '../container';
 import { LogoIcon } from '../svgs/logo-icon';
 import { RiContactsLine } from 'react-icons/ri';
 import Drawer from './drawer';
+import { useEffect, useState } from 'react';
 
 export function NavBar() {
+    const [isSticky, setIsSticky] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <Container>
-            <div className="relative z-50">
-                <nav className="flex justify-between items-center py-5">
-                    <div className='flex items-center gap-12'>
-                        <Link href={""}>
-                            <LogoIcon width={85} height={80} />
-                        </Link>
-                        <ul className='sm:flex gap-6 hidden'>
-                            <li>
-                                <Link href={"#start"} className='nav-link'>
-                                    Início
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href={"#who"} className='nav-link'>
-                                    Quem somos?
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href={"#solutions"} className='nav-link'>
-                                    Soluções
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className='flex items-center gap-6'>
-                        <div className="relative h-12 w-40 rounded-xl group">
-                            <div className="absolute inset-0 bg-gradient-to-r from-[#73BFFF] to-[#9A35E4] transition-transform duration-300 ease-in-out transform scale-90 group-hover:scale-x-[1.03] group-hover:scale-y-[1.1] rounded-xl" />
-                            <Link href={""} className="relative flex items-center gap-1 shadow-2xl justify-center w-full h-full rounded-xl bg-white text-black z-10">
-                                <RiContactsLine className='text-lg' />
-                                <p>Fale conosco</p>
+        <div className={`px-14 w-full ${isSticky ? "fixed shadow-lg bg-white" : "relative"} z-50 flex transition-all duration-300`}>
+            <nav className={`w-full flex justify-between items-center ${isSticky ? "py-0" : "py-5"}`}>
+                <div className='flex items-center gap-12'>
+                    <Link href={""}>
+                        <LogoIcon width={isSticky ? 50 : 85} height={80} />
+                    </Link>
+                    <ul className='sm:flex gap-6 hidden'>
+                        <li>
+                            <Link href={"#start"} className='nav-link'>
+                                Início
                             </Link>
-                        </div>
-                        <Drawer />
+                        </li>
+                        <li>
+                            <Link href={"#who"} className='nav-link'>
+                                Quem somos?
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={"#solutions"} className='nav-link'>
+                                Soluções
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex items-center gap-6'>
+                    <div className="relative h-12 w-40 rounded-xl group">
+                        <div className="absolute inset-0 bg-gradient-to-r from-[#73BFFF] to-[#9A35E4] transition-transform duration-300 ease-in-out transform scale-90 group-hover:scale-x-[1.03] group-hover:scale-y-[1.1] rounded-xl" />
+                        <Link href={""} className="relative flex items-center gap-1 shadow-2xl justify-center w-full h-full rounded-xl bg-white text-black z-10">
+                            <RiContactsLine className='text-lg' />
+                            <p>Fale conosco</p>
+                        </Link>
                     </div>
-                </nav>
-            </div>
-        </Container>
+                    <Drawer />
+                </div>
+            </nav>
+        </div>
     );
 }
