fa99bae61b6df8b689b0ab271094a4f4f3c79e7c
Mikael <mikaelpeixoto123t@gmail.com>
2025-01-02 02:01:18 -0300

    Refactor MainContent component to improve layout structure, enhance text visibility, and update styling for better user experience

--- src/components/main/main-content.tsx
+++ src/components/main/main-content.tsx
@@ -12,7 +12,7 @@ import RocketImage from '../../../public/rocket.png';
 
 export default function MainContent() {
     const [isRocketFloating, setIsRocketFloating] = useState(false); // State to control the rocket floating animation.
-    
+
     return (
         <>
             <Container className="flex flex-col gap-12">
@@ -22,13 +22,24 @@ export default function MainContent() {
                             DECOLE O POTENCIAL <br />DA SUA EMPRESA <br />COM A MAXYNI
                         </h1>
 
-                        <p className='text-white text-medium mt-2'>
-                            Somos especialistas em criar soluções digitais para empresas e pessoas. Transformamos suas ideias em realidade com qualidade, segurança e agilidade. <br />
-                            Nossos diferenciais nos destacam no mercado. Clique em <strong>Decolar</strong> e descubra como podemos impulsionar seus resultados!
-                        </p>
+                        <div className="flex flex-col what-we-do mt-2 gap-1">
+                            <div>
+                                <p className='text-white text-medium'>
+                                    Somos especialistas em criar soluções digitais para empresas e pessoas.
+                                </p>
+
+                                <p className='text-white text-medium hidden lg:block'>
+                                    Transformamos suas ideias em realidade com qualidade, segurança e agilidade.
+                                </p>
+                            </div>
+
+                            <p className="text-white text-medium">
+                                Nossos diferenciais nos destacam no mercado. Clique em <strong>Decolar</strong> e descubra como podemos impulsionar seus resultados!
+                            </p>
+                        </div>
 
                         <div className="relative h-12 w-40 rounded-xl group mt-5">
-                            <div className="absolute inset-0 shadow-white shadow-md bg-sky-500 transition-transform duration-300 ease-in-out transform scale-90 group-hover:scale-x-[1.03] group-hover:scale-y-[1.1] rounded-xl" />
+                            <div className="absolute inset-0 shadow-white shadow-md bg-sky-300 transition-transform duration-300 ease-in-out transform scale-90 group-hover:scale-x-[1.03] group-hover:scale-y-[1.1] rounded-xl" />
                             <Link
                                 href={"#solutions"}
                                 className="relative flex items-center gap-2 shadow-2xl justify-center w-full h-full rounded-xl bg-white text-black z-10">
@@ -37,9 +48,10 @@ export default function MainContent() {
                             </Link>
                         </div>
                     </div>
+
                     <div
                         className={`hidden lg:block animate-[rocketEntry_1.5s_ease-out_forwards] ${isRocketFloating && "animate-[rocketFloat_3s_ease-in-out_infinite]"}`}
-                        onAnimationEnd={() => setIsRocketFloating(true)} // When the rocket entry animation ends, start the floating animation.
+                        onAnimationEnd={() => setIsRocketFloating(true)} // When the rocket entry animation ends, starts the floating animation.
                     >
                         <Image
                             src={RocketImage}
@@ -49,15 +61,9 @@ export default function MainContent() {
                     </div>
                 </div>
 
-                <div className="mt-12">
+                <div className="flex flex-col gap-24 mt-10">
                     <AboutMaxyni />
-                </div>
-
-                <div className='mt-12'>
                     <BestChoice />
-                </div>
-
-                <div className='mt-12'>
                     <Solutions />
                 </div>
             </Container>
